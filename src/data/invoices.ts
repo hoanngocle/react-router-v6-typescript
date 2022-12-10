d8273ba3f8f359e515a5792ba72e8e8f83b57dd9
hoanln <hoanngocle.93@gmail.com>
2022-12-10 16:47:11 +0700

    Add nested route

---
+++ src/data/invoices.ts
@@ -0,0 +1,19 @@
+interface Invoice {
+  id: number,
+  name: string
+}
+
+export const invoices: Invoice[] = [
+  {
+    id: 1,
+    name: 'Google'
+  },
+  {
+    id: 2,
+    name: 'Yahoo'
+  },
+  {
+    id: 3,
+    name: 'Apple'
+  }
+]
--- src/pages/Invoices.tsx
+++ src/pages/Invoices.tsx
@@ -1,8 +1,23 @@
+import { Link } from 'react-router-dom';
+import { invoices } from '../data/invoices';
+
 const Invoices = () => {
   return (
     <main>
       <h2>Invoices</h2>
+      <div style={{ display: 'flex' }}>
+        <nav style={{ padding: '1rem', borderRight: '1px solid' }}>
+          {invoices.map((invoice) => (
+            <Link
+              to={`/invoices/${invoice.id}`}
+              key={invoice.id}
+              style={{ display: 'block', margin: '1rem' }}>
+              {invoice.name}
+            </Link>
+          ))}
+        </nav>
+      </div>
     </main>
-  )
-}
-export default Invoices
+  );
+};
+export default Invoices;
